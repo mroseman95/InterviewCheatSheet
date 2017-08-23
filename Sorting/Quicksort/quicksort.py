f3d0f781c016234b42e24b5b831e7497c8e03dfd
Matthew Roseman <mroseman95@gmail.com>
2017-08-23 16:56:38 -0400

    added a median of three method to choose a pivot, so there are two variations of quicksort possible
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- Sorting/Quicksort/quicksort.py
+++ Sorting/Quicksort/quicksort.py
@@ -7,6 +7,9 @@ def quicksort(A, l=None, r=None):
     @param l: The leftmost index of the sublist
     @param r: The rightmost index of the sublist
     @return: A list that contains the same elements of A but in sorted order
+    O(nlogn) where n is len(A) if quicksort median of medians is used, but O(n^2) if median of three is used
+    but median of three is faster on average, because choosing a pivot is O(c) time instead of O(n) time
+    like in median of median method
     """
     if l is None:
         l = 0
@@ -31,6 +34,7 @@ def partition(A, l, r):
     @param l: The leftmost index of the sublist
     @param r: The rightmost index of the sublist
     @return: The index of the partition number
+    O(n) + O(n) = O(n) where n = r - l
     """
     p = set_pivot(A, l, r)
     i = l
@@ -53,6 +57,57 @@ def set_pivot(A, l, r):
     @param l: The leftmost index of the sublist
     @param r: The rightmost index of the sublist
     @return: The value of the pivot
+    O(n/5 * 5) = O(n) where n = r - l
+    """
+    #pivot = get_pivot_median_of_three(A, l, r)
+    pivot = get_pivot_median_of_medians(A, l, r)
+    pivot_index = l + A[l:r+1].index(pivot)
+
+    # swap index p and r
+    A[pivot_index], A[r] = A[r], A[pivot_index]
+
+    return pivot
+
+def get_pivot_median_of_three(A, l, r):
+    """
+    get_pivot_median_of_three takes a sublist of A and finds a pivot within it using the median of three method
+    @param A: A list of elements that can be compared to each other
+    @param l: the leftmost index of the sublist
+    @param r: the rightmost index of the sublist
+    @return: the valud of the pivot
+    """
+    # If there are less than 3 elements
+    if r  + 1 - l <= 3:
+        return sorted(A[l:r+1])[(r + 1 - l) // 2]
+
+    random.seed(time.time())
+    # the middle candidate is chosen from the sublist minus the first and last element
+    candidate_2 = random.randrange(l+1,r)
+    # if there is only one element to the left of the second candidate, make that the first candidate
+    if candidate_2 - l == 1:
+        candidate_1 = l
+    else:
+        candidate_1 = random.randrange(l, candidate_2)
+    # if there is only one element to the right of the second candidate, make that the third candidate
+    if r+1 - candidate_2 == 1:
+        candidate_3 = r+1
+    else:
+        candidate_3 = random.randrange(candidate_2+1, r+1)
+
+    candidate_values = sorted([A[candidate_1], A[candidate_2], A[candidate_3]])
+    A[candidate_1] = candidate_values[0]
+    A[candidate_2] = candidate_values[1]
+    A[candidate_3] = candidate_values[2]
+
+    return A[candidate_2]
+
+def get_pivot_median_of_medians(A, l, r):
+    """
+    get_pivot_median_of_medians takes a sublist of A and finds a pivot within it using the median of medians method
+    @param A: A list of elements that can be compared to each other
+    @param l: the leftmost index of the sublist
+    @param r: the rightmost index of the sublist
+    @return: the value of the pivot
     """
     medians = []
     for i in xrange(l, r + 1, 5):
@@ -60,9 +115,5 @@ def set_pivot(A, l, r):
         medians.append(temp[len(temp) // 2])
 
     pivot = sorted(medians)[len(medians) // 2]
-    pivot_index = l + A[l:r+1].index(pivot)
-
-    # swap index p and r
-    A[pivot_index], A[r] = A[r], A[pivot_index]
 
     return pivot
